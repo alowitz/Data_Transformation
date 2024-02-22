f45ff4de621967eab628529d389b609b067cc9bc
Austen Lowitz <austenlowitz@gmail.com>
2024-02-21 23:19:59 -0500

    chart

--- transform.py
+++ transform.py
@@ -2,10 +2,11 @@ import streamlit as st
 import pandas as pd
 import re
 import datetime
+import plotly.express as px
 
 # Streamlit app configuration
-st.set_page_config(page_title="Data Transformation Tool", page_icon='👩‍🔬')  # You can use an emoji as an icon
-st.header("Data Transformation Tool for Bio Nerds 🧪")
+st.set_page_config(page_title="Data Transformation Tool", page_icon='🧬')  # You can use an emoji as an icon
+st.header("Data Transformation Tool for Bio Nerds 👩‍🔬🧪🧬")
 
 # Get today's date
 Today = datetime.date.today()
@@ -78,7 +79,53 @@ if uploaded_file is not None:
 
         # Display the final corrected DataFrame
         st.write('Preview of the transformed data:')
-        st.dataframe(final_df)
+        st.dataframe(final_df.head())
+        st.markdown('---')
+        
+        st.subheader("Now let's make a plot!")
+        for i in range(1, len(final_df.columns), num_groups):
+            # Get the group name by splitting the column name and removing the last part (e.g., "_1", "_2", ...)
+            group_name = '_'.join(final_df.columns[i].split('_')[:-1])
+
+            # Select the columns that belong to the current group
+            group_columns = final_df.columns[i:i + num_groups]
+
+            # Calculate the mean and standard errors across the selected columns for each row and create a new column for the mean
+            final_df[f'{group_name}_mean'] = final_df[group_columns].mean(axis=1)
+            final_df[f'{group_name}_sem'] = final_df[group_columns].sem(axis=1)
+        
+        #Define aggregated df, x, and y values for the plot
+        agg_df = final_df[['Sample']+[col for col in final_df if 'mean' in col or 'sem' in col]]
+        x = list(final_df['Sample'].unique())
+        y = [col.replace('_mean','') for col in final_df if 'mean' in col]
+        st.write(agg_df)
+        
+        col1, col2 = st.columns(2)
+
+        # 'x' multiselect in the first column
+        with col1:
+            x_selection = st.multiselect('Select Sample(s) for the X-Axis', x, key='x_multiselect')
+
+        # 'y' multiselect in the second column
+        with col2:
+            y_selection = st.multiselect('Select Column(s) you want displayed for the Y-Axis', y, key='y_multiselect')
+            y_selection = [f'{col}_mean' for col in y_selection]
+            
+            
+        if x_selection and y_selection:
+
+            # Filter the DataFrame based on the selected x values
+            chart_df = agg_df[agg_df['Sample'].isin(x_selection)]
+            # Create a list of columns to include in the chart DataFrame
+            chart_columns = ['Sample'] + y_selection
+            chart_df = chart_df[chart_columns]
+            st.write(chart_df)
+            
+            fig = px.bar(chart_df, x='Sample', y=y_selection, barmode='group', labels = {'value':'% of Cells (Mean)'})
+            st.plotly_chart(fig)
+            
+        else:
+            st.write("Please select at least one value for X and Y.")
 
         csv = final_df.to_csv(index=False)
         st.download_button(
